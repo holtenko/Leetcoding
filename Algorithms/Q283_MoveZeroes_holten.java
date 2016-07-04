46d0742e3ce15bcbe703d5ead56df2ff1dad6ffe
holten.gao <holten.gao@vipshop.com>
2016-07-04 11:23:00 +0800

    add one more solution to Q283. Move Zeroes in the Leetcode

--- Algorithms/Q283_MoveZeroes_holten.java
+++ Algorithms/Q283_MoveZeroes_holten.java
@@ -20,3 +20,18 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0,j=0;
+        while(i<nums.length){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+            i++;
+        }
+        while(j<nums.length){
+            nums[j++]=0;
+        }
+    }
+}
