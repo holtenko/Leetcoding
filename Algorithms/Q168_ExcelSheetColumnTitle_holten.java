34e941cd9a8262f839ae0bfb4e23f95a91824203
holten.gao <holten.gao@vipshop.com>
2016-07-19 14:11:27 +0800

    Excel Sheet Column Title

---
+++ Algorithms/Q168_ExcelSheetColumnTitle_holten.java
@@ -0,0 +1,12 @@
+/**
+* Created by holten on 2016-07-19.
+* Email: holten.ko@gmail.com
+*/
+
+public class Solution {
+    public String convertToTitle(int n) {
+        if (n <= 26) return (char)('A' + n - 1) + "";
+        if (n % 26 != 0) return convertToTitle((n / 26)) + (char)('A' + (n % 26) - 1);
+        else return convertToTitle((n / 26) - 1) + "Z";
+    }
+}
