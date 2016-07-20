23fa30f37a569971eadcc03096cc4ea4322cf498
holten.gao <holten.gao@vipshop.com>
2016-07-20 10:50:48 +0800

    217_ContainsDuplicate

---
+++ Algorithms/Q217_ContainsDuplicate_holten.java
@@ -0,0 +1,24 @@
+/**
+* Created by holten on 2016-07-20.
+* Email: holten.ko@gmail.com
+*/
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int min=Integer.MAX_VALUE;
+		int max=Integer.MIN_VALUE;
+		for(int tmp:nums){
+			if(tmp<min) min=tmp;
+			if(tmp>max) max=tmp;
+		}
+		boolean[] boolMap=new boolean[max-min+1];
+		for(int tmp:nums){
+			if(boolMap[tmp-min]) {
+				return true;
+			}else{
+				boolMap[tmp-min]=true;
+			}
+		}
+		return false;
+    }
+}
