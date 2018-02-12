362a4382d5f183d6a4e315d59edb10f1b808a54e
holten <holten.ko@gmail.com>
2018-02-12 10:37:16 +0800

    771. Jewels and Stones

---
+++ Algorithms/Q771_JewelsAndStones_holten.java
@@ -0,0 +1,22 @@
+/**
+ * Created by holten on 2018-02-12.
+ * Email: holten.ko@foxmail.com
+ */
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int result = 0;
+        char[] jArray = J.toCharArray();
+        HashSet jSet = new HashSet();
+        for (char j : jArray) {
+            jSet.add(j);
+        }
+        char[] sArray = S.toCharArray();
+        for (char s : sArray) {
+            if (jSet.contains(s)) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
