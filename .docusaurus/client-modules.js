84bbe09b34867c3be2fd73676d46ba25f4469c83
holten <holten.ko@gmail.com>
2022-10-29 16:57:23 +0800

    add docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/holten/Workspace/Leetcoding/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/holten/Workspace/Leetcoding/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/holten/Workspace/Leetcoding/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/holten/Workspace/Leetcoding/src/css/custom.css'),
+];
---
+++ docs/13-backtracking/num1.md
@@ -0,0 +1,8 @@
+---
+sidebar_label: 'Dashboard'
+sidebar_position: 1
+---
+
+# Manage Docs Versions
+
+Docusaurus can manage multiple versions of your docs.
